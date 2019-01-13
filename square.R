8129ae4b8f04b05a03338c210ac65b61a48b3899
MikeKozelMSU <kozelmi1@msu.edu>
2019-01-13 13:50:44 -0500

    Add files via upload
    file upload

---
+++ square.R
@@ -0,0 +1,22 @@
+#' Square a number
+#'
+#' takes x and squares it
+#'
+#' @author Mike Kozel
+#'
+#' @param x a numeric variable to be squared
+#' @return the square of the input
+#' @export
+
+
+square <- function(x){
+  return(x^2)
+}
+
+#' cube a number
+#' @param X a number to be cubed
+#' @return  the cube of the input
+#' @export
+cube <- function(x){
+  return(x^3)
+}
